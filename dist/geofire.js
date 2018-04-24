e9095431076e6fa1b0504fec57bdbce91aeda5bc
Anthony Maldarelli <amaldare93@gmail.com>
2018-04-24 10:46:26 -0400

    pass raw location data to event callbacks

--- dist/geofire.js
+++ dist/geofire.js
@@ -790,7 +790,7 @@ var GeoQuery = function (firebaseRef, queryCriteria) {
    * @param {string} key The key of the geofire location.
    * @param {?Array.<number>} location The location as [latitude, longitude] pair.
    */
-  function _updateLocation(key, location) {
+  function _updateLocation(key, location, locationData) {
     validateLocation(location);
     // Get the key and location
     var distanceFromCenter, isInQuery;
@@ -811,11 +811,11 @@ var GeoQuery = function (firebaseRef, queryCriteria) {
 
     // Fire the "key_entered" event if the provided key has entered this query
     if (isInQuery && !wasInQuery) {
-      _fireCallbacksForKey("key_entered", key, location, distanceFromCenter);
+      _fireCallbacksForKey("key_entered", key, location, distanceFromCenter, locationData);
     } else if (isInQuery && oldLocation !== null && (location[0] !== oldLocation[0] || location[1] !== oldLocation[1])) {
-      _fireCallbacksForKey("key_moved", key, location, distanceFromCenter);
+      _fireCallbacksForKey("key_moved", key, location, distanceFromCenter, locationData);
     } else if (!isInQuery && wasInQuery) {
-      _fireCallbacksForKey("key_exited", key, location, distanceFromCenter);
+      _fireCallbacksForKey("key_exited", key, location, distanceFromCenter, locationData);
     }
   }
 
@@ -862,7 +862,8 @@ var GeoQuery = function (firebaseRef, queryCriteria) {
    * @param {Firebase DataSnapshot} locationDataSnapshot A snapshot of the data stored for this location.
    */
   function _childAddedCallback(locationDataSnapshot) {
-    _updateLocation(getKey(locationDataSnapshot), decodeGeoFireObject(locationDataSnapshot.val()));
+    var locationDataVal = locationDataSnapshot.val()
+    _updateLocation(getKey(locationDataSnapshot), decodeGeoFireObject(locationDataVal), locationDataVal);
   }
 
   /**
@@ -871,7 +872,8 @@ var GeoQuery = function (firebaseRef, queryCriteria) {
    * @param {Firebase DataSnapshot} locationDataSnapshot A snapshot of the data stored for this location.
    */
   function _childChangedCallback(locationDataSnapshot) {
-    _updateLocation(getKey(locationDataSnapshot), decodeGeoFireObject(locationDataSnapshot.val()));
+    var locationDataVal = locationDataSnapshot.val()
+    _updateLocation(getKey(locationDataSnapshot), decodeGeoFireObject(locationDataVal), locationDataVal);
   }
 
   /**
